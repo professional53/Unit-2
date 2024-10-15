3a46266bb72eada47e33819f9fd3347852fc605f
professional53 <149533887+professional53@users.noreply.github.com>
2024-10-15 14:23:43 -0700

    Completed Examples

--- src/word_games/WordGamesTester.java
+++ src/word_games/WordGamesTester.java
@@ -6,10 +6,12 @@ public class WordGamesTester {
         Scanner input = new Scanner(System.in);
 
         // Ask for a word
-
+        System.out.println("Enter a word: ");
+        WordGames WordTest = new WordGames(input.nextLine());
+        System.out.println(WordTest);
         // Scramble it
         // Print out scrambled word
-
+        System.out.println(WordTest.scramble());
         // Ask for an index
         // Ask for random word
         System.out.println("Enter an integer: ");
@@ -20,12 +22,18 @@ public class WordGamesTester {
 
         // Add random word at index
         // Print out the word
+        System.out.println(WordTest.bananaSplit(idx, word));
 
 
         // Ask for a character (store as a String)
         // Ask for random word
         // Add random word at character
         // Print out the word
+        System.out.println("Enter a character: ");
+        String character = input.nextLine();
+        System.out.println("Enter another word: ");
+        String ranword = input.nextLine();
+        System.out.print(WordTest.bananaSplit(character, ranword));
 
     }
 }
